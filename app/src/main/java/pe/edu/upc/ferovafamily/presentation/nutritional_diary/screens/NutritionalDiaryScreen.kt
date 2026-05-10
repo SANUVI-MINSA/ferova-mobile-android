a26c1cfed8ec119d19f6fcfa2fb1502417b2c303
MaineMa <144504783+MaineMa@users.noreply.github.com>
2026-05-09 22:59:52 -0500

    feat: Initialize NutritionalDiaryScreen
    * **Nutritional Diary**: Created the `NutritionalDiaryScreen.kt` file with a placeholder Composable and preview to begin the nutritional tracking module.

---
+++ app/src/main/java/pe/edu/upc/ferovafamily/presentation/nutritional_diary/screens/NutritionalDiaryScreen.kt
@@ -0,0 +1,15 @@
+package pe.edu.upc.ferovafamily.presentation.nutritional_diary.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun NutritionalDiaryScreen() {
+
+}
+
+@Preview
+@Composable
+fun NutritionalDiaryPreview() {
+    NutritionalDiaryScreen()
+}
