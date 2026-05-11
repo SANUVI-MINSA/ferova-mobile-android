f600d46cd1374664644efbc07f9b16cd071ae0a1
MaineMa <144504783+MaineMa@users.noreply.github.com>
2026-05-11 01:05:10 -0500

    refactor: Remove unused imports in TipCard.kt
    Removed unused imports for `FontStyle`, `FontWeight`, and `sp` in the `TipCard` component to clean up the code.

--- app/src/main/java/pe/edu/upc/ferovafamily/presentation/nutritional_diary/components/TipCard.kt
+++ app/src/main/java/pe/edu/upc/ferovafamily/presentation/nutritional_diary/components/TipCard.kt
@@ -1,6 +1,5 @@
 package pe.edu.upc.ferovafamily.presentation.nutritional_diary.components
 
-import android.graphics.fonts.FontStyle
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -21,9 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import pe.edu.upc.ferovafamily.R
 import pe.edu.upc.ferovafamily.presentation.theme.Crimson
 
