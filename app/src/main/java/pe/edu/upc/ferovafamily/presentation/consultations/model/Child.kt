5c860b977196f8934fe3133a0c87927b2c7dc893
Ariana Ramirez <ariana190406@gmail.com>
2026-06-17 09:52:35 -0500

    refactor(communication): remove legacy Child presentation model

--- app/src/main/java/pe/edu/upc/ferovafamily/presentation/consultations/model/Child.kt
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.ferovafamily.presentation.consultations.model
-
-data class Child(
-    val id: String,
-    val name: String,
-    val age: Int,
-    val description: String = ""
-)
